bd96d2b235a7eaac200c5ecec33b7bf968bdf1b8
DuduPOG <eduardo.aguiar@escolar.ifrn.edu.br>
2025-04-09 17:30:37 +0000

    tirei arquivos duplicados

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Olá mundo!");
+﻿namespace OlaMundo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine(); // Lê a linha inteira, incluindo espaços
+
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine()); // Lê a linha e converte para inteiro
+
+        Console.WriteLine($"Olá, {nome}! Você tem {idade} anos.");
+    }
+}
