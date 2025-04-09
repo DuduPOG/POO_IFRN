9e7fedcecc35107065b01d9821ee1375ffdfaa0b
DuduPOG <eduardo.aguiar@escolar.ifrn.edu.br>
2025-04-09 14:01:09 +0000

    ola mundo em c++ no vscode

--- OlaMundo/obj/Debug/net8.0/OlaMundo.AssemblyInfo.cs
+++ OlaMundo/obj/Debug/net8.0/OlaMundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685e0425bda8f28bf84db2308f010bbe3cbd4ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a1a043a2a2ae0ffb1ee0209086c50202d7af087")]
 [assembly: System.Reflection.AssemblyProductAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
