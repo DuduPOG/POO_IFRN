28670d4d022950f1648c7a9edaf45f179a8a5908
DuduPOG <eduardo.aguiar@escolar.ifrn.edu.br>
2025-04-28 21:06:42 +0000

    esqueci de mencionar no commit anterior que retirei arquivos inúteis e consertei emu programinha java!

--- Aulas_POO/main/OlaMundo.java
+++ Aulas_POO/main/OlaMundo.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class OlaMundo {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int idade;
-            String nome;
+        Scanner sc = new Scanner(System.in);
+        int idade;
+        String nome;
             
-            System.out.println("Digite seu nome: ");
-            nome = sc.next();
-            System.out.println("Digite sua idade: ");
-            idade = sc.nextInt();
-            System.out.printf("Olá, %s! Você tem %d anos%n.", nome, idade);
-        }
+        System.out.println("Digite seu nome: ");
+        nome = sc.next();
+        System.out.println("Digite sua idade: ");
+        idade = sc.nextInt();
+        System.out.printf("Olá, %s! Você tem %d anos%n.", nome, idade);
+        sc.close();
     }
 }
